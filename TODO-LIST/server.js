e421c9b384a01ba98d13a6af0a7b49a231f6539e
sushmajb <jb.sushma15@gmail.com>
2016-05-17 16:31:11 +0530

    To-do list using nodejs

---
+++ TODO-LIST/server.js
@@ -0,0 +1,80 @@
+//setup...........
+
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+//configuration........
+
+//mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');
+mongoose.connect('mongodb://localhost/todolist');
+app.use(express.static(__dirname));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(methodOverride());
+
+//define model......
+
+var Todo = mongoose.model('Todo', {
+    text: String
+});
+    
+//routes......
+
+app.get('/api/todos',function(req,res) {
+    Todo.find(function(err, todos) {
+        if(err)
+            res.send(err)
+        res.json(todos);
+        });    
+    });
+    
+    
+app.post('/api/todos',function(req,res) {
+    Todo.create({
+        text : req.body.text,
+        done :false
+        }, function(err, todo) {
+            if(err)
+                res.send(err);
+            Todo.find(function(err, todos) {
+                if(err)
+                    res.send(err);
+                res.json(todos);
+                });
+        });    
+    });
+    
+ app.delete('/api/todos/:todo_id', function(req,res) {
+    Todo.remove({
+        _id : req.params.todo_id
+    }, function(err, todo) {
+        if(err)
+            res.send(err);
+        
+        Todo.find(function(err, todos) {
+            if(err)
+                res.send(err);
+            res.json(todos);
+            });
+       
+        });
+    
+    });
+    
+//application......
+
+ app.get('*', function(req, res) {
+    res.sendFile('./index.html');
+ });
+    
+
+//listen(star app with node server.js)......
+app.listen(8080);
+console.log("App listening on port 8080");
+
